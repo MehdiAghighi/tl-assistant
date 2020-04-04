26c841ce710c5958e53a0d836b5c8cab818fb5e9
MehdiAghighi <mamaliosezar@gmail.com>
2020-04-04 19:09:52 +0430

    added ability to get chat ids with sending !!id in them also changed the read and id messages destination to 13727337953 channel (Message Reading )

--- assistant.py
+++ assistant.py
@@ -29,21 +29,36 @@ async def newMessageSent(event):
     user = await event.get_chat()
     if command == 'id':
         if event.is_private:
-            await client.send_message("me", "[" + user.first_name + "]" + "(" + "https://t.me/" + user.username + ")" + " : " + "`" + str(user.id) + "`", link_preview=False)
+            await client.send_message(1372737953, "[" + user.first_name + "]" + "(" + "https://t.me/" + user.username + ")" + " : " + "`" + str(user.id) + "`", link_preview=False)
+            return
         if event.is_reply:
             replyed_message = await event.get_reply_message()
             replyed_user = await client.get_entity(replyed_message.from_id)
-            await client.send_message("me", "[" + replyed_user.first_name + "]" + "(" + "https://t.me/" + replyed_user.username + ")" + " : " + "`" + str(replyed_user.id) + "`", link_preview=False)
+            await client.send_message(1372737953, "[" + replyed_user.first_name + "]" + "(" + "https://t.me/" + replyed_user.username + ")" + " : " + "`" + str(replyed_user.id) + "`", link_preview=False)
+            return
+        if event.is_channel:
+            print(user.stringify())
+            await client.send_message(1372737953, user.title + " : " + "`" + str(user.id) + "`", link_preview=False)
+            return
+        if event.is_group:
+            await client.send_message(1372737953, user.title + " : " + "`" + str(user.id) + "`", link_preview=False)
+            return
 
 
 @events.register(events.MessageRead(func=lambda e: e.is_private))
 async def messageRead(event):
     user = await event.get_chat()
     message = await client.get_messages(user, ids=event.max_id)
-    await client.send_message("me", "[" + user.username.capitalize() + "]" + "(" + "https://t.me/" + user.username + ")" + " Has Read My Message " + "👇", link_preview=False)
-    await message.forward_to("me")
+    await client.send_message(1372737953, "[" + user.username.capitalize() + "]" + "(" + "https://t.me/" + user.username + ")" + " Has Read My Message " + "👇", link_preview=False)
+    await message.forward_to(1372737953)
+
+
+@events.register(events.NewMessage())
+async def newMessage(event):
+    print(await event.get_chat())
 
 client.start()
 client.add_event_handler(newMessageSent)
 client.add_event_handler(messageRead)
+# client.add_event_handler(newMessage)
 client.run_until_disconnected()
